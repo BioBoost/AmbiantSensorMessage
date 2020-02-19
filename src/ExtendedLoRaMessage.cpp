68e7caf14ba8baa207b3290ccca74dda42b5e704
Maxim Vanden Abeele <maxim.vandenabeele@gmail.com>
2020-02-19 11:28:20 +0100

    add namespace

--- src/ExtendedLoRaMessage.cpp
+++ src/ExtendedLoRaMessage.cpp
@@ -1,30 +1,32 @@
 #include "ExtendedLoRaMessage.h"
 
-ExtendedLoRaMessage& ExtendedLoRaMessage::addTemperature(double temperature)
-{
-    reallocBuffer2(2).writeTemperature(temperature);
-    return *this;
-}
-
-ExtendedLoRaMessage& ExtendedLoRaMessage::addHumidity(double humidity)
-{
-    if (humidity < 0) {
-        humidity = 0;
-    } else if (humidity > 100) {
-        humidity = 100;
+namespace ExtendedLoRaMessage {
+    ExtendedLoRaMessage& ExtendedLoRaMessage::addTemperature(double temperature)
+    {
+        reallocBuffer(2).writeTemperature(temperature);
+        return *this;
     }
-    reallocBuffer2(2).writeHumidity(humidity);
-    return *this;
-}
 
-ExtendedLoRaMessage& ExtendedLoRaMessage::addPressure(double pressure)
-{
-    reallocBuffer2(2).writePressure(pressure);
-    return *this;
-}
+    ExtendedLoRaMessage& ExtendedLoRaMessage::addHumidity(double humidity)
+    {
+        if (humidity < 0) {
+            humidity = 0;
+        } else if (humidity > 100) {
+            humidity = 100;
+        }
+        reallocBuffer(2).writeHumidity(humidity);
+        return *this;
+    }
 
-ExtendedLoRaMessage& ExtendedLoRaMessage::addPM(uint16_t pm)
-{
-  reallocBuffer2(2).writePM(pm);
-  return *this;
+    ExtendedLoRaMessage& ExtendedLoRaMessage::addPressure(double pressure)
+    {
+        reallocBuffer(2).writePressure(pressure);
+        return *this;
+    }
+
+    ExtendedLoRaMessage& ExtendedLoRaMessage::addPM(uint16_t pm)
+    {
+    reallocBuffer(2).writePM(pm);
+    return *this;
+    }
 }
--- src/ExtendedLoRaMessage.h
+++ src/ExtendedLoRaMessage.h
@@ -3,12 +3,16 @@
 #include "../mbed-lorawan-serialization/src/LoRaMessage.h"
 #include "ExtendedLoRaEncoder.h"
 
-class ExtendedLoRaMessage: public LoRaMessage{
-    public:
-        ExtendedLoRaMessage& addTemperature(double temperature);
-        ExtendedLoRaMessage& addHumidity(double humidity);
-        ExtendedLoRaMessage& addPressure(double pressure);
-        ExtendedLoRaMessage& addPM(uint16_t pm);
+namespace ExtendedLoRaMessage {
+    class ExtendedLoRaMessage: public LoRaMessage{
+        public:
+            ExtendedLoRaMessage();
 
-        ExtendedLoRaEncoder reallocBuffer2(int delta);
-};
+            ExtendedLoRaMessage& addTemperature(double temperature);
+            ExtendedLoRaMessage& addHumidity(double humidity);
+            ExtendedLoRaMessage& addPressure(double pressure);
+            ExtendedLoRaMessage& addPM(uint16_t pm);
+
+            ExtendedLoRaEncoder reallocBuffer(int delta);
+    };
+}
