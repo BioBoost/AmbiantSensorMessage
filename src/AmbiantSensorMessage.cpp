483554a9ee7da58dfaa1d4f67699b8b6ac924a23
Maxim Vanden Abeele <maxim.vandenabeele@gmail.com>
2020-02-19 21:48:21 +0100

    add buffer and size

--- src/AmbiantSensorMessage.cpp
+++ src/AmbiantSensorMessage.cpp
@@ -6,6 +6,7 @@ namespace AmbiantSensorMessage {
     LoRaMessage& AmbiantSensorMessage::addTemperature(int temperature)
     {
         return loRaMessage.addUint16(temperature);
+        buffer += 2;
     }
 
     void AmbiantSensorMessage::addHumidity(int humidity)
@@ -16,15 +17,28 @@ namespace AmbiantSensorMessage {
             humidity = 100;
         }
         loRaMessage.addUint16(humidity);
+        buffer += 2;
     }
 
     void AmbiantSensorMessage::addPressure(int pressure)
     {
         loRaMessage.addUint16(pressure);
+        buffer += 2;
     }
 
     void AmbiantSensorMessage::addPM(int pm)
     {
         loRaMessage.addUint16(pm);
+        buffer += 2;
+    }
+
+    uint8_t* AmbiantSensorMessage::getMessage()
+    {
+        return buffer;
+    }
+    
+    uint8_t AmbiantSensorMessage::getLength()
+    {
+        return size;
     }
 }
--- src/AmbiantSensorMessage.h
+++ src/AmbiantSensorMessage.h
@@ -12,5 +12,12 @@ namespace AmbiantSensorMessage {
             LoRaMessage& addTemperature(int temperature);
             void addPressure(int pressure);
             void addPM(int pm);
+
+            uint8_t* getMessage();
+            uint8_t getLength();
+        
+        private:
+            uint8_t* buffer = 0;
+            uint8_t size = 0;
     };
 }
